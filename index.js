9243f7cf1309c8de5f47aa3527e2f976beeb84b8
ezeey123 <ebubsdeb101@gmail.com>
2024-11-08 07:28:37 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("user-menu-button").addEventListener("click", function () {
+    document.getElementById("menu-dropdown").classList.toggle("hidden");
+});
+
+
+document.getElementById("mobile-menu-btn").addEventListener("click", function () {
+    document.getElementById("mobile-dropdown").classList.toggle("hidden");
+});
